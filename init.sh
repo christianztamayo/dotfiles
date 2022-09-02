f4c36bc9a4f8f2645c0c0c085d4777f8894b3a90
Christian Z. Tamayo <christianztamayo@gmail.com>
2022-09-02 18:17:07 +0800

    Fix diff-so-fancy on git interactive add

--- init.sh
+++ init.sh
@@ -103,6 +103,7 @@ COMMAND="    # basic config
     git config --global rerere.enabled true
 
     # diff-so-fancy
+    git config --global add.interactive.useBuiltin false
     git config --global core.pager \"diff-so-fancy | less --tabs=4 -RFX\"
     git config --global interactive.diffFilter \"diff-so-fancy --patch\"
     git config --global color.ui true
